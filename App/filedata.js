cd7d6b5228a32130e0fcad3053cb7f7b95f8b4ed
akibo567 <akibo567@live.jp>
2021-11-24 00:42:57 +0900

    GitQuestと連携できるようになった

--- App/filedata.js
+++ App/filedata.js
@@ -8,11 +8,11 @@ class Filedata {
         this.birthtime = "";
     }
     return_json() {
-        let res = "{\n\tname:"+ this.name+ ",\n"
-        +"\tpath:" + this.path+ ",\n"
-        +"\tsha:" + this.sha+ ",\n"
-        +"\tsize:" + this.size+ ",\n"
-        +"\ttype:" + this.type+ "\n"
+        let res = "{\n\tname: "+ this.name+ ",\n"
+        +"\tpath: " + this.path+ ",\n"
+        //+"\tsha:" + this.sha+ ",\n"
+        +"\tsize: " + this.size+ ",\n"
+        +"\ttype: " + this.type+ "\n"
         + "}";
 
         return res;
@@ -20,39 +20,3 @@ class Filedata {
 }
 
 module.exports = Filedata;
-
-
-/*
-  {
-    "name": ".shellcheckrc",
-    "path": ".shellcheckrc",
-    "sha": "bc119bdcafca653d1f548190f7f6a385ee7d26a5",
-    "size": 45,
-    "url": "https://api.github.com/repos/acidanthera/OpenCorePkg/contents/.shellcheckrc?ref=master",
-    "html_url": "https://github.com/acidanthera/OpenCorePkg/blob/master/.shellcheckrc",
-    "git_url": "https://api.github.com/repos/acidanthera/OpenCorePkg/git/blobs/bc119bdcafca653d1f548190f7f6a385ee7d26a5",
-    "download_url": "https://raw.githubusercontent.com/acidanthera/OpenCorePkg/master/.shellcheckrc",
-    "type": "file",
-    "_links": {
-      "self": "https://api.github.com/repos/acidanthera/OpenCorePkg/contents/.shellcheckrc?ref=master",
-      "git": "https://api.github.com/repos/acidanthera/OpenCorePkg/git/blobs/bc119bdcafca653d1f548190f7f6a385ee7d26a5",
-      "html": "https://github.com/acidanthera/OpenCorePkg/blob/master/.shellcheckrc"
-    }
-  },
-  {
-    "name": "AppleModels",
-    "path": "AppleModels",
-    "sha": "5c66789ca13edcce96bb99cb70293613663e13ad",
-    "size": 0,
-    "url": "https://api.github.com/repos/acidanthera/OpenCorePkg/contents/AppleModels?ref=master",
-    "html_url": "https://github.com/acidanthera/OpenCorePkg/tree/master/AppleModels",
-    "git_url": "https://api.github.com/repos/acidanthera/OpenCorePkg/git/trees/5c66789ca13edcce96bb99cb70293613663e13ad",
-    "download_url": null,
-    "type": "dir",
-    "_links": {
-      "self": "https://api.github.com/repos/acidanthera/OpenCorePkg/contents/AppleModels?ref=master",
-      "git": "https://api.github.com/repos/acidanthera/OpenCorePkg/git/trees/5c66789ca13edcce96bb99cb70293613663e13ad",
-      "html": "https://github.com/acidanthera/OpenCorePkg/tree/master/AppleModels"
-    }
-  },
-*/
--- App/server.js
+++ App/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors');
 const app = express()
 const port = 3000
 
@@ -7,55 +8,64 @@ const port = 3000
 const Filedata = require('./filedata.js');
 const fs = require('fs');
 const dir_path = "./Localrepo";
-const proj_name = "OpenCorePkg";
+const proj_name = "hello-world";
+const proj_dir = "";
 
-var res_string = "[";
 
 
-app.get("/", (req, res, next) => {
+app.use(cors({
+    origin: 'http://localhost:8080', //アクセス許可するオリジン
+    credentials: true, //レスポンスヘッダーにAccess-Control-Allow-Credentials追加
+    optionsSuccessStatus: 200 //レスポンスstatusを200に設定
+}));
+
+
+app.get("/contents", (req, res, next) => {
     // アクセスログ
     console.log(req.method, req.url) 
-    // jsonを返す
-    //res.json(require('./responce.json'))
+
+    //ヘッダ
     res.header('Content-Type', 'application/json; charset=utf-8')
 
-    var obj_list =[];
-    res_string = "[";
+    let obj_list =[];
+
+    let proj_name = req.query.repo;
+    let proj_path = req.query.path ? req.query.path + "/" : "";
 
 
-    files = fs.readdirSync(dir_path + "/" + proj_name);
+
+
+    files = fs.readdirSync(dir_path + "/" + proj_name + "/"+ proj_path);
     files.forEach(function(file_name){ 
         let obj = new Filedata();
-        var fullPath = dir_path + '/'+ proj_name+ '/'+ file_name; // フルパスを取得
-        var stats = fs.statSync(fullPath) // ファイル（またはフォルダ）の情報を取得
+        let fullPath = dir_path + '/'+ proj_name+ "/"+ proj_path +file_name; // フルパスを取得
+        let stats = fs.statSync(fullPath) // ファイル（またはフォルダ）の情報を取得
         obj.name = file_name;
-        obj.path = fullPath;
+        obj.path = proj_path +file_name;
+        obj.size = stats.size;
 
         if(stats.isDirectory()){ // フォルダの場合
             //getFiles(fullPath); // getFilesを再帰的に呼び出し
+            obj.type = "dir";
         }else{ // ファイルの場合
             // ファイル情報を取得
-            text = file_name // ファイル名
+            /*text = file_name // ファイル名
                 + ',' + stats.size // ファイルサイズ：単位バイト
                 + ',' + stats.birthtime.getFullYear() + '/' // ファイル作成日：年
                 + ('0' + (stats.birthtime.getMonth() + 1)).slice(-2) + '/' // ファイル作成日：月
                 + ('0' + stats.birthtime.getDate()).slice(-2) // ファイル作成日；日
                 + '\n'; 
-            console.log(text);
-            obj.size = stats.size;
+            console.log(text);*/
+            obj.type = "file";
         }
         obj_list.push(obj);
     });
 
-    //console.log(flst);
-    obj_list.forEach(function(obj){
-        res_string += "\n"+obj.return_json() + ",";
-    });
+    res.send(obj_list);
+});
 
-
-    res_string += "]";
-    res.send(res_string);
-
+app.get("/json_test", (req, res, next) => {
+    res.json(require('../responce.json'))
 });
 
 app.post('/pst', function(req, res) {
